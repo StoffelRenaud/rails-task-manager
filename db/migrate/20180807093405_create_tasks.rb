1b4e2b168155fda1860c3ff9230051964e41bf7d
RenaudStoffel <stoffelrenaud@gmail.com>
2018-08-07 19:36:41 +0200

    task manager done

--- db/migrate/20180807093405_create_tasks.rb
+++ db/migrate/20180807093405_create_tasks.rb
@@ -1,8 +1,8 @@
 class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
-      t.string :title
-      t.text :details
+      t.string  :title
+      t.text    :details
       t.boolean :completed, default: false
       t.timestamps
     end
